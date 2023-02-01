8e9eedcc27a64cb34be79d55c2d55be24141ed9e
David Marcano <dfmr96@gmail.com>
2023-02-01 14:32:39 -0300

    Gravity fix Horizontal movement working

--- GGJ Game/Assets/PlayerController.cs
+++ GGJ Game/Assets/PlayerController.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] float speed = 5;
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] int jumpForce = 10;
+    [SerializeField] Vector2 jumpVector;
+    [SerializeField] float moveX;
+    [SerializeField] Vector2 velocity;
+    private void Start()
     {
-        
+        Application.targetFrameRate = 60;
+        rb = GetComponent<Rigidbody2D>();
+        jumpVector = new Vector2(0, jumpForce);
+
+    }
+    private void Update()
+    {
+        moveX = Input.GetAxis("Horizontal");
+        velocity = rb.velocity;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        HorizontalMovement();
+    }
+    void HorizontalMovement()
+    {
+        rb.velocity = new Vector2(moveX * speed * Time.fixedDeltaTime, rb.velocity.y);
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(jumpVector);
+        }
     }
 }
