8455474a7a3aba65dabf37cacd51acccabe53864
David Marcano <dfmr96@gmail.com>
2023-02-02 16:45:00 -0300

    Sap Management Added

---
+++ GGJ Game/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public int maxHealth;
+    public int minHealth;
+    [SerializeField] int _health;
+    public int health 
+    {
+        get { return _health; }
+        private set { _health = value;} 
+    }
+
+    public int maxSap;
+    public int minSap;
+    [SerializeField] int _sap;
+    public int sap 
+    {
+        get { return _sap; }
+        private set { _sap = value;}
+    }
+
+    public void ReduceSap()
+    {
+        if (sap <= 0)
+        {
+            health--;
+        }
+        else
+        {
+            sap--;
+        }
+    }
+}
