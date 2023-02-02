4ca9fac538a84ea0a654a8a5569e9fdb4b02cea3
David Marcano <dfmr96@gmail.com>
2023-02-02 17:13:07 -0300

    Bullet now deal damage Enemy can take damage

--- GGJ Game/Assets/Scripts/BulletController.cs
+++ GGJ Game/Assets/Scripts/BulletController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class BulletController : MonoBehaviour
 {
+    [SerializeField] int damage;
     [SerializeField] float speed;
     [SerializeField] float destroyDelay = 2f;
 
@@ -23,6 +24,16 @@ public class BulletController : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        DealDamage(collision.gameObject);
         Destroy(gameObject);
+    }
+
+    void DealDamage(GameObject target)
+    {
+        if (target.CompareTag("Enemy"))
+        {
+            Debug.Log(target.tag);
+            target.GetComponent<Enemy>().TakeDamage(damage);
+        }
     }
 }
