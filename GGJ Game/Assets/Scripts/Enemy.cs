056eef422d99a4b825802b957920f58970eab1c6
David Marcano <dfmr96@gmail.com>
2023-02-03 15:40:15 -0300

    Enemy can chase player

--- GGJ Game/Assets/Scripts/Enemy.cs
+++ GGJ Game/Assets/Scripts/Enemy.cs
@@ -13,11 +13,14 @@ public class Enemy : MonoBehaviour
     [SerializeField] float changeDirectionTimer;
     [SerializeField] float attackCooldown;
     [SerializeField] float attackTimer;
+    [SerializeField] bool isChasing;
+    [SerializeField] Vector3 movDirection;
 
     private void Start()
     {
         attackTimer = 0;
         changeDirectionTimer = 0;
+        movDirection = transform.right;
     }
     private void Update()
     {
@@ -31,15 +34,18 @@ public class Enemy : MonoBehaviour
 
     void HorizontalMovement()
     {
-        transform.Translate(transform.right * speed * Time.deltaTime);
+        if (!isChasing)
+        {
+        transform.Translate(movDirection * speed * Time.deltaTime);
+        }
     }
 
     void ChangeDirection()
     {
-        if((enemyCollider.bounds.max.x >= movementArea.bounds.max.x || enemyCollider.bounds.min.x <= movementArea.bounds.min.x) && changeDirectionTimer >= changeDirectionCooldown)
+        if((enemyCollider.bounds.max.x >= movementArea.bounds.max.x || enemyCollider.bounds.min.x <= movementArea.bounds.min.x) && changeDirectionTimer >= changeDirectionCooldown && !isChasing)
         {
             changeDirectionTimer = 0;
-            speed = -speed;
+            movDirection = -movDirection;
         }
     }
 
@@ -52,9 +58,29 @@ public class Enemy : MonoBehaviour
     {
         if (attackTimer >= attackCooldown && collision.gameObject.CompareTag("Player"))
         {
+            speed = 0;
             Debug.Log("Jugador dañado");
             attackTimer = 0;
             collision.gameObject.GetComponent<PlayerStats>().TakeDamage(damage);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Vector3 playerDir = collision.gameObject.transform.position - transform.position;
+            Debug.Log($"{gameObject.name} is chasing Player");
+            isChasing = true;
+            transform.Translate(new Vector3((playerDir.x),0,0).normalized * speed * Time.deltaTime);
+        }
+    }
+    
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isChasing = false;
         }
     }
 }
