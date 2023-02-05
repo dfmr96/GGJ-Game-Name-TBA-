073cd69d4fd3208ba903a460c04991e05717cfca
dfmr96 <dfmr96@gmail.com>
2023-02-04 22:49:45 -0300

    Spider trigger added

--- GGJ Game/Assets/Scripts/SpiderObjectsController.cs
+++ GGJ Game/Assets/Scripts/SpiderObjectsController.cs
@@ -1,10 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SpiderObjectsController : MonoBehaviour
 {
     [SerializeField] GameObject[] doorsToClose;
     [SerializeField] GameObject[] doorsLever;
-    [SerializeField] GameObject spiderActivator;
+
+    public void HideObjectsLevers()
+    {
+        for (int i = 0; i < doorsLever.Length; i++)
+        {
+            doorsLever[i].SetActive(false);
+        }
+    }
 }
